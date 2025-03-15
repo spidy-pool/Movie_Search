06d5c80e23bac1989a20b03c59749d95d2d5c652
Parvendra Singh <119687954+spidy-pool@users.noreply.github.com>
2025-03-15 23:42:19 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FaHeart } from 'react-icons/fa';
 import { BiSolidCameraMovie } from "react-icons/bi";
 import { RiMovie2AiLine  } from "react-icons/ri";
 
 const Header = () => {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
     return(
-         <div className="flex items-center justify-center mb-8 p-2 bg-gray-300">
+         <div className={`flex items-center ${screenWidth >540 ? 'justify-center' : ""} mb-8  bg-gray-300`}>
         <BiSolidCameraMovie className="text-5xl mr-3 text-yellow-600 animate-pulse" />
         <Link to="/" className="text-4xl font-extrabold tracking-wide">
             MovieSearch
         </Link>
-        <RiMovie2AiLine className="text-3xl ml-3 text-yellow-600 animate-pulse" />
+        { screenWidth >540 && <RiMovie2AiLine className="text-3xl ml-3 text-yellow-600 animate-pulse" />}
+        {/* <RiMovie2AiLine className="text-3xl ml-3 text-yellow-600 animate-pulse" /> */}
         <Link to="/favourite-movies" className="absolute right-6 text-5xl"><FaHeart /></Link>
     </div>
     )};
